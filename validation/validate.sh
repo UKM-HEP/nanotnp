7fc4507872cfc37e895dec75f95b5cd1f6a63d6e
SiewYan Hoh <siew.yan.hoh@gmail.com>
2023-01-29 22:40:26 +0800

    template for nanoTrimmer dev

--- validation/validate.sh
+++ validation/validate.sh
@@ -5,8 +5,8 @@ python plot.py \
        -n 2012runb_ele \
        -l 4.43 \
        -e 8TeV \
-       -f 0.06 \
+       -f 10. \
        -p e \
        -r \
        ${PWD}/../../nanoTrimmer/DYJetsToLL_M-50_TuneZ2Star_trimmed/DYJetsToLL_M-50_TuneZ2Star.root \
-       ${PWD}/../../nanoTrimmer/Run2012B_SingleElectron_trimmed/Run2012B_SingleElectron.root
+       ${PWD}/../../nanoTrimmer/Run2012A_SingleElectron_trimmed/Run2012A_SingleElectron.root
