2fb7ea40066db45c2586e1136c0da6a3644847ec
SiewYan <siew.yan.hoh@gmail.com>
2023-02-02 15:14:29 +0800

    fix directory

--- analysis/etc/inputs/tnpOpenData.py
+++ analysis/etc/inputs/tnpOpenData.py
@@ -26,7 +26,8 @@ datasets = {
     'DYJetsToLL_M-50_TuneZ2Star_zainol' : tnpSample('DYJetsToLL_M-50_TuneZ2Star', opendataDir + 'zainol/DYJetsToLL_M-50_TuneZ2Star_trimmed/DYJetsToLL_M-50_TuneZ2Star.root', isMC = True, nEvts = -1 ),
 }
 
-testDir = '/disk01/cms-open-data/trimmed_v1/zainol/'
+#testDir = '/disk01/cms-open-data/trimmed_v1/zainol/'
+testDir = '/home/jovyan/data/trimmed_v1/zainol/'
 tests = {
     'Run2012B_SingleMu' : tnpSample('Run2012B_SingleMu', testDir + 'Run2012B_SingleMu_trimmed/Run2012B_SingleMu.root' , lumi = 4.43 ),
     'DYJetsToLL_M-50_TuneZ2Star' : tnpSample('DYJetsToLL_M-50_TuneZ2Star', testDir + 'DYJetsToLL_M-50_TuneZ2Star_trimmed/DYJetsToLL_M-50_TuneZ2Star.root', isMC = True, nEvts = -1 )
